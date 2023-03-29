1579d416adbd70c00e2cebdb8b45d07166749927
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2023-03-29 11:42:52 -0700

    Prototype 3 completed
    All I have to do as of today are the bonus features, Challenge 3, and the bonus features for Challenge 3.

--- Unit_3/Assets/Prototype 3/Scripts/PlayerController.cs
+++ Unit_3/Assets/Prototype 3/Scripts/PlayerController.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator playerAnimator;
     private Rigidbody playerRB;
+    private AudioSource playerAudio;
     public float jumpForce;
     public float gravityModifier;
     public bool isGrounded = true;
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+            playerAnimator.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -27,11 +38,17 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            dirtParticle.Play();
         }
-        if (collision.gameObject.CompareTag("Obstacle"))
+        else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
